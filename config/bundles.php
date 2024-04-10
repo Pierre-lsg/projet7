e4038c780a084e7fd2852223a2881d62f7ba7804
Pierre Bonnafous <pierre.bonnafous@gmail.com>
2024-04-10 21:28:11 +0200

    add toggle ux stuff and db-tools ... not sure if it's useful

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    MakinaCorpus\DbToolsBundle\DbToolsBundle::class => ['all' => true],
+    Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
 ];
